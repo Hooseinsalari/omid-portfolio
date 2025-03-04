2df13b0aa7859c26c2013cbae322683ad3ada17d
Hossein <hossein.salari002@tasu.ventures>
2025-03-04 13:32:13 +0330

    init

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeHero from "./components/HomeHero";
+
+function ViewHome() {
+  return (
+    <div>
+      <HomeHero />
+    </div>
+  );
+}
+
+export default ViewHome;
